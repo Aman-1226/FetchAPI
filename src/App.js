470b8c856c0eb461f5858c0a6c08b9b72f481077
Aman Kumar Sahu <86238740+Aman-1226@users.noreply.github.com>
2023-08-08 13:21:35 +0530

    Refactoring the Code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Show from './components/Show';
-// import Booking from './components/Bookingi'; // Import the Booking component
 import Bookingitems from './components/Bookingitems';
 
 const App = () => {
